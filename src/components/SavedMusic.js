6084db857e6c48ded93315a62cd8f13692401e4e
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-09-13 16:10:06 -0400

    fetch test

--- src/components/SavedMusic.js
+++ src/components/SavedMusic.js
@@ -1,4 +1,13 @@
+import {useState, useEffect} from "react"
+
+
 function SavedMusic () {
+    fetch("http://localhost:3001/artists")
+    .then(res => res.json())
+    .then(savedMusicArray => console.log(savedMusicArray))
+
+
+
     return <h1 className="page-header">Saved Music</h1>
 }
 
