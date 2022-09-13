4bf94a705f52cb15d4c7e7595d9444d4e6b96714
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-09-13 17:22:02 -0400

    saved music and saved music card init

--- src/components/SavedMusic.js
+++ src/components/SavedMusic.js
@@ -1,14 +1,27 @@
+import SavedMusicCard from "./SavedMusicCard";
 import {useState, useEffect} from "react"
 
 
 function SavedMusic () {
-    fetch("http://localhost:3001/artists")
-    .then(res => res.json())
-    .then(savedMusicArray => console.log(savedMusicArray))
+
+    const [savedMusicArray, setSavedMusicArray] = useState([])
+    useEffect(()=> {
+        fetch("http://localhost:3001/artists")
+        .then(res => res.json())
+        .then(data => setSavedMusicArray(data))},[]);
+
+        console.log(savedMusicArray);
+
+    const cards = savedMusicArray.map((savedArrayItem) => {
+        return <SavedMusicCard key={savedArrayItem.name} savedArrayItem={savedArrayItem} />
+        })
 
 
-
-    return <h1 className="page-header">Saved Music</h1>
-}
+    return (
+        <>
+        <h1 className="page-header">Saved Music</h1>
+        {cards}
+        </>
+)}
 
 export default SavedMusic;
---
+++ src/components/SavedMusicCard.js
@@ -0,0 +1,8 @@
+function SavedMusicCard({savedArrayItem}) {
+    return(
+        <div className="saved-music-card">
+            <h1>{savedArrayItem.name}</h1>
+        </div>
+    )
+}
+export default SavedMusicCard
